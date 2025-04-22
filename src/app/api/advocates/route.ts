859116c1822951465194ec423a25fb1ba64e0cb2
Sam J Hill <samuhill@gmail.com>
2025-04-22 13:13:09 -0400

    backend search and pagination (#3)
    * switch to using the real backend
    
    * add pagination
    
    * working lastName search
    
    * add search for other fields

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,43 @@
+import { asc, ilike, or } from "drizzle-orm";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(req: Request) {
+  const { searchTerm = '', limit = 10, page = 1 } = req.url
+    .split('?')[1]
+    ?.split('&')
+    .reduce((params: Record<string, string>, param) => {
+      const [key, value] = param.split('=');
+      params[key] = value;
+      return params;
+    }, {});
 
-  const data = advocateData;
+  const skip = (Number(page) - 1) * Number(limit);
 
-  return Response.json({ data });
+  try {
+    const data = await db
+      .select()
+      .from(advocates)
+      .where(searchTerm ?
+        or(
+          ilike(advocates.firstName, `%${searchTerm}%`),
+          ilike(advocates.lastName, `%${searchTerm}%`),
+          ilike(advocates.city, `%${searchTerm}%`),
+          ilike(advocates.degree, `%${searchTerm}%`),
+          // TODO ilike(advocates.phoneNumber, `%${searchTerm}%`)
+        )
+      : undefined)
+      .orderBy(asc(advocates.id))
+      .limit(limit)
+      .offset(skip); 
+
+    return Response.json({
+      data,
+      totalAdvocates: data.length,
+      totalPages: Math.ceil(Number(data.length) / Number(limit)),
+    });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: 'Failed to fetch advocates' }, { status: 500 });
+  }
 }
